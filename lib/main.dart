78b106d2a8acdaa47c7e433a04654411afe361ab
gunumdogdu <emirgunumdogdu@gmail.com>
2023-04-02 14:02:34 +0300

    provider, state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,87 @@
 import 'package:flutter/material.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import 'providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner:
-          false, //REMOVED DEBUG BANNER FOR AEST. PURPOSES
-      title: 'MyShop',
-      theme: ThemeData(
-        canvasColor: Colors.lightBlue.shade100,
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.deepOrange,
-        ).copyWith(
-          secondary: Colors.lightGreen,
-          surface: Colors.grey,
-          error: Colors.red,
-        ),
-        textTheme: ThemeData.light().textTheme.copyWith(
-              bodyLarge: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontFamily: 'Anton',
-              ),
-              bodySmall: TextStyle(
-                color: Colors.grey,
-                fontSize: 14,
-              ),
-              bodyMedium: TextStyle(
-                  color: Color.fromRGBO(250, 249, 246, 1),
-                  fontWeight: FontWeight.bold,
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner:
+            false, //REMOVED DEBUG BANNER FOR AEST. PURPOSES
+        title: 'MyShop',
+        theme: ThemeData(
+          canvasColor: Colors.lightBlue.shade100,
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.deepOrange,
+          ).copyWith(
+            secondary: Colors.lightGreen,
+            surface: Colors.grey,
+            error: Colors.red,
+          ),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                bodyLarge: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontFamily: 'Anton',
+                ),
+                bodySmall: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+                bodyMedium: TextStyle(
+                    color: Color.fromRGBO(250, 249, 246, 1),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    fontFamily: 'Anton'),
+                titleLarge: TextStyle(
+                  color: Colors.black,
+                  // fontWeight: FontWeight.bold,
+                  fontFamily: 'Anton',
+                  fontStyle: FontStyle.italic,
                   fontSize: 16,
-                  fontFamily: 'Anton'),
-              titleLarge: TextStyle(
-                color: Colors.black,
-                // fontWeight: FontWeight.bold,
-                fontFamily: 'Anton',
-                fontStyle: FontStyle.italic,
-                fontSize: 16,
+                ),
+                displayMedium: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Anton',
+                  fontSize: 16,
+                ),
+                displayLarge: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.deepPurple),
               ),
-              displayMedium: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Anton',
-                fontSize: 16,
-              ),
-              displayLarge: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.deepPurple),
-            ),
-        appBarTheme: AppBarTheme(
-          titleSpacing: 0,
-          elevation: 0,
-          backgroundColor: Colors.deepOrange,
-          iconTheme: IconThemeData(
-            color: Colors.white,
-          ),
-          actionsIconTheme: IconThemeData(
-            color: Colors.amber,
-          ),
-          //DID NOT UNDERSTAND
-          titleTextStyle: TextStyle(
+          appBarTheme: AppBarTheme(
+            titleSpacing: 0,
+            elevation: 0,
+            backgroundColor: Colors.deepOrange,
+            iconTheme: IconThemeData(
               color: Colors.white,
-              fontFamily: 'Lato',
-              fontSize: 24,
-              fontWeight: FontWeight.bold),
-          centerTitle: true,
+            ),
+            actionsIconTheme: IconThemeData(
+              color: Colors.amber,
+            ),
+            //DID NOT UNDERSTAND
+            titleTextStyle: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Lato',
+                fontSize: 24,
+                fontWeight: FontWeight.bold),
+            centerTitle: true,
+          ),
         ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/providers/products.dart';
+
+import './product_item.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 3.4,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (context, i) => ProductItem(
+        products[i].id,
+        products[i].title,
+        products[i].imageUrl,
+      ),
+    );
+  }
+}
