6ddd8e886ac3579ef013dbdd14a1451c2345580d
gunumdogdu <emirgunumdogdu@gmail.com>
2023-04-05 21:54:01 +0300

    snackbar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -53,6 +53,22 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Item added to cart!',
+                  ),
+                  duration: Duration(
+                    seconds: 2,
+                  ),
+                  action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      }),
+                ),
+              );
             },
             color: Theme.of(context).colorScheme.secondary,
           ),
