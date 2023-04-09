f00a5a51a2981196cf1b91562f8b80322974ca37
gunumdogdu <emirgunumdogdu@gmail.com>
2023-04-09 03:23:09 +0300

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shop_app/providers/orders.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import '../env.dart';
 
 class Auth with ChangeNotifier {
   String? _token;
@@ -8,8 +9,9 @@ class Auth with ChangeNotifier {
   String? _userId;
 
   Future<void> signup(String email, String password) async {
+    print(apiKey);
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA8Kdel8EL4TaqJSsGbt28siAgYP8RJKq4');
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey');
     final response = await http.post(
       url,
       body: json.encode(
