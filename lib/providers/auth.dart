76b9372d2325d1edb64715ddbfa439b12be7374d
gunumdogdu <emirgunumdogdu@gmail.com>
2023-04-09 20:41:15 +0300

    allowing users to login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,10 +8,10 @@ class Auth with ChangeNotifier {
   DateTime? _expiryDate;
   String? _userId;
 
-  Future<void> signup(String email, String password) async {
-    print(apiKey);
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$apiKey');
     final response = await http.post(
       url,
       body: json.encode(
@@ -25,5 +25,13 @@ class Auth with ChangeNotifier {
     print(
       json.decode(response.body),
     );
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
